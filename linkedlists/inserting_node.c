56900ecbfe13b6d897d173feb56a2950e3e49b90
arvind14nitt <arvind14nitt@gmail.com>
2022-09-24 20:56:40 +0530

    LinkedList - Insert at Head, End and after some element in List

---
+++ linkedlists/inserting_node.c
@@ -0,0 +1,98 @@
+/*Inserting a node at the front, at the end and at the given position in a given linked list*/
+
+#include "stdio.h"
+#include "stdlib.h"
+
+/* data type for the node */
+struct node{
+   int data;
+   struct node * next;
+};
+
+void printList(struct node* head)
+{
+    while(head)
+    {
+       printf("%d->",head->data);
+       head = head->next;
+    }
+    printf("NULL\n");
+}
+
+
+struct node* create_node(int data)
+{
+   struct node* new_node =(struct node *)malloc(sizeof(struct node));
+   new_node->data = data;
+   new_node->next = NULL;
+   return new_node;
+}
+
+void insertAtHead(int data, struct node** head)
+{
+   struct node * new_node = create_node(data);
+   new_node->next = (*head);
+   *head = new_node;
+}
+
+void insertAtEnd(struct node* head, int data)
+{
+   struct node* new_node = create_node(data);
+   struct node * last_node;
+   while(head){
+      last_node = head;
+      head = head->next;
+   }
+   last_node->next = new_node;
+}
+
+void insertAtIndex(struct node * head, int key_element, int data)
+{
+   struct node *key_node;
+   struct node* new_node = create_node(data);
+   //find the node which has the key_element
+   while(head)
+   {
+      if(head->data == key_element)
+      {
+          key_node = head;
+	  new_node->next = key_node->next;
+	  key_node->next = new_node;
+	  break;
+      }
+      head = head->next;
+   }
+}
+/* 1->2->3->4->5->NULL */
+
+int main()
+{
+   struct node * head, *temp, *temp_prev;
+   int i,data=0;
+   head = create_node(1);
+   temp = head;
+   temp_prev = temp;
+   //create node, change the next for each node to point to next node
+   for(i=2;i<=5;i++){
+	temp = create_node(i);
+	temp_prev->next = temp;
+	temp_prev = temp;
+   }
+   printList(head);
+   //Insert 0 at the head
+   insertAtHead(data, &head);//why address of head, because we want to modify the head 
+   printList(head); /* 0->1->2->3->4->5->NULL */
+
+   //Insert 6 at the end
+   data = 6;
+   insertAtEnd(head, data);//just pass the head, no need to modify the head
+   printList(head);/*0->1->2->3->4->5->6->NULL */
+   //Insert 99 after 3
+   int key = 3;
+   data = 99;
+   insertAtIndex(head, key, data);/*0->1->2->3->99->4->5->6->NULL */
+   printList(head);
+   return 0;
+}
+
+
